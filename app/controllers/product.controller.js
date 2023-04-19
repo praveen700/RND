a700c436a5dc676cd5e7d637edb3b1a32ad1c7f4
praveen.mr <praveen.mr@datafoundry.ai>
2023-04-19 17:41:30 +0530

    removed async

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -1,8 +1,8 @@
 const sql = require("../models/db");
 const qu = require("../Querys/products.query");
 module.exports = {
-    fetchProductgroup: async (req, res, next) => {
-        await sql.query(qu.fetchQuery(), (err, data) => {
+    fetchProductgroup:  (req, res, next) => {
+         sql.query(qu.fetchQuery(), (err, data) => {
             if (err) {
                 res.status(403).json({ error: err.message });
             } else {
@@ -10,8 +10,8 @@ module.exports = {
             }
         })
     },
-    createProductGroup: async (req, res, next) => {
-        await sql.query(qu.insertProducts(req.body), (err, data) => {
+    createProductGroup:  (req, res, next) => {
+         sql.query(qu.insertProducts(req.body), (err, data) => {
             if (err) {
                 res.status(403).json({ error: err.message });
             } else {
@@ -19,8 +19,8 @@ module.exports = {
             }
         })
     },
-    updateProductGroup: async (req, res, next) => {
-        await sql.query(qu.updateProducts(req.body,req.params.id), (err, data) => {
+    updateProductGroup:  (req, res, next) => {
+         sql.query(qu.updateProducts(req.body,req.params.id), (err, data) => {
             if (err) {
                 res.status(403).json({ error: err.message });
             } else {
@@ -28,8 +28,8 @@ module.exports = {
             }
         })
     },
-    deleteProductGroup: async (req, res, next) => {
-        await sql.query(qu.deleteProductGroupQuery(req.params.id), (err, data) => {
+    deleteProductGroup:  (req, res, next) => {
+         sql.query(qu.deleteProductGroupQuery(req.params.id), (err, data) => {
             if (err) {
                 res.status(403).json({ error: err.message });
             } else {
